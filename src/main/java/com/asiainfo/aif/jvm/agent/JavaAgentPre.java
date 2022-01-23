34bfc64c05c10eb98dda39fc8cffe41a531fb864
0027017605 <weng.jiaxing@iwhalecloud.com>
2022-01-23 22:36:13 +0800

    合并台式机

---
+++ src/main/java/com/asiainfo/aif/jvm/agent/JavaAgentPre.java
@@ -0,0 +1,11 @@
+package com.asiainfo.aif.jvm.agent;
+
+import java.lang.instrument.Instrumentation;
+
+public class JavaAgentPre {
+    public static void premain(String args, Instrumentation inst){
+        System.out.println("premain start");
+        //System.out.println(args);
+        inst.addTransformer(new JavaAgentTransformer());
+    }
+}
---
+++ src/main/java/com/asiainfo/aif/jvm/gc/JavaObjectHeapFoo.java
@@ -0,0 +1,9 @@
+package com.asiainfo.aif.jvm.gc;
+
+import com.asiainfo.aif.jvm.agent.TimeTest;
+
+public class JavaObjectHeapFoo {
+    public static void main(String[] args) {
+        TimeTest.sayHello();
+    }
+}
