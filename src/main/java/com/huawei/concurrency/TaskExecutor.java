41e5de19f6d15f97686e3f35f2debfa103ce9618
0027017605 <weng.jiaxing@iwhalecloud.com>
2022-02-13 20:15:13 +0800

    并发测试

--- src/main/java/com/huawei/concurrency/TaskExecutor.java
+++ src/main/java/com/huawei/concurrency/TaskExecutor.java
@@ -1,7 +1,7 @@
-package com.huawei.concurrency.task;
+package com.huawei.concurrency;
 
 import java.util.List;
-import java.util.UUID;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -9,59 +9,65 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import com.huawei.entity.Ticket;
+import com.huawei.concurrency.task.TicketRunnable;
 import com.huawei.concurrency.thread.NamedThreadFactory;
 
 /**
- * 任务抽象
+ * 任务处理器
  * @author WJX
  */
-public class SellTask {
-	private LinkedBlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(1);
+public class TaskExecutor {
+	private LinkedBlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(10);
 
 	/**
-	 * 初始化任务
+	 * 如果运行的线程多于 corePoolSize 而少于 maximumPoolSize，会首选将任务runnable添加到队列，当队列满时才创建新线程
 	 */
-    public SellTask(Runnable r) {
-		//使用put向队尾添加元素，如果队列中任务已满，会发生阻塞一直等待空间，以加入元素。
-		addTask(r);
+	private ExecutorService executor;
+
+	public TaskExecutor() {
+		initialize();
 	}
 
+	void initialize() {
+		executor = new ThreadPoolExecutor(
+				4, //核心线程数
+				8, //最大线程数
+				60L, //
+				TimeUnit.SECONDS,
+				workQueue,//这里的队列用于暂时存放任务，当有空闲线程时取出执行
+				new NamedThreadFactory("Sell Thread", new Thread.UncaughtExceptionHandler() {
+					@Override
+					public void uncaughtException(Thread t, Throwable e) {
+						System.out.println(t.getName()+" execute failed.");
+						e.printStackTrace();
+					}
+				})) {
+		};
+	}
 
-    /**
-     * 如果运行的线程多于 corePoolSize 而少于 maximumPoolSize，会首选将任务runnable添加到队列，当队列满时才创建新线程
-     */
-	private ExecutorService executor = new ThreadPoolExecutor(
-			1, //核心线程数
-			1, //最大线程数
-			60L, //
-			TimeUnit.SECONDS,
-			workQueue,//这里的队列用于暂时存放任务，当有空闲线程时取出执行
-			new NamedThreadFactory(UUID.randomUUID().toString(), new Thread.UncaughtExceptionHandler() {
-
-				@Override
-				public void uncaughtException(Thread t, Throwable e) {
-					System.out.println(t.getName()+" execute failed.");
-					e.printStackTrace();
-				}
-			})) {
-
-	};
+	/**
+	 * 往队列中添加任务
+	 */
+	public void addTask(TicketRunnable task){
+		try {
+			workQueue.put(task);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * 启动任务
 	 */
 	public void start() {
-
-		if (!workQueue.isEmpty()) {
+		while (!workQueue.isEmpty()) {
 		    try {
                 executor.execute(workQueue.take());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-		}else{
-		    stop();
 		}
+		stop();
 	}
 
 	/**
@@ -102,24 +108,9 @@ public class SellTask {
         return futures;
     }
 
-
-
-	/**
-	 * 往队列中添加任务
-	 */
-	private void addTask(Runnable task){
-		try {
-			workQueue.put(task);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-
 	/**
 	 * 关闭任务
 	 */
-
 	public void stop() {
 		executor.shutdown();
 		try
--- src/main/java/com/huawei/concurrency/thread/NamedThreadFactory.java
+++ src/main/java/com/huawei/concurrency/thread/NamedThreadFactory.java
@@ -1,18 +1,19 @@
 package com.huawei.concurrency.thread;
 
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
 public class NamedThreadFactory implements ThreadFactory {
-
-	private String tname;
+	private final Random random = new Random(10);
+	private String tName;
 	private Thread thread;
 	private ThreadFactory defaultThreadFactory = Executors.defaultThreadFactory();
 	private UncaughtExceptionHandler uncaughtExceptionHandler;
 
-	public NamedThreadFactory(String tname, UncaughtExceptionHandler uncaughtExceptionHandler) {
-		this.tname = tname;
+	public NamedThreadFactory(String tName, UncaughtExceptionHandler uncaughtExceptionHandler) {
+		this.tName = tName;
 		this.uncaughtExceptionHandler = uncaughtExceptionHandler;
 	}
 
@@ -24,7 +25,7 @@ public class NamedThreadFactory implements ThreadFactory {
 			thread = new Thread(r);
 		}
 		thread.setUncaughtExceptionHandler(uncaughtExceptionHandler);
-		thread.setName(tname);
+		thread.setName(tName + random.nextInt());
 		return thread;
 
 	}
