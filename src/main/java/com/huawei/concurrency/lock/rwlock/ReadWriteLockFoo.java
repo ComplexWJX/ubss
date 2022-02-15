efa96782dd442cbc78b3759f7db451cd5efbe747
0027017605 <weng.jiaxing@iwhalecloud.com>
2022-02-15 20:29:41 +0800

    读写锁

--- src/main/java/com/huawei/concurrency/lock/rwlock/ReadWriteLockFoo.java
+++ src/main/java/com/huawei/concurrency/lock/rwlock/ReadWriteLockFoo.java
@@ -10,18 +10,23 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  */
 public class ReadWriteLockFoo {
     private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    private final Lock readLock = readWriteLock.readLock();
+
+    private final Lock writeLock = readWriteLock.writeLock();
+
     public void read() throws InterruptedException {
-        Lock readLock = readWriteLock.readLock();
         try {
             readLock.lockInterruptibly();
+            String name = Thread.currentThread().getName();
+            if (Integer.parseInt(name.substring(6)) <=3)
             Thread.sleep(2000);
-            System.out.println(Thread.currentThread().getName() + "read");
+            System.out.println(name + " read");
         }finally {
             readLock.unlock();
         }
     }
     public void write() throws InterruptedException {
-        Lock writeLock = readWriteLock.writeLock();
         try {
             writeLock.lockInterruptibly();
             System.out.println(Thread.currentThread().getName() + "write");
@@ -32,25 +37,36 @@ public class ReadWriteLockFoo {
 
     public static void main(String[] args) {
         ReadWriteLockFoo foo = new ReadWriteLockFoo();
-        Thread t1 = new Thread(() -> {
-            try {
-                foo.read();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        Thread t2 = new Thread(() -> {
-            try {
-                foo.write();
-                foo.read();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        t1.setName("t1");
-        t2.setName("t2");
-        t1.start();
-        t2.start();
+//        Thread t1 = new Thread(() -> {
+//            try {
+//                foo.read();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        Thread t2 = new Thread(() -> {
+//            try {
+//                //foo.write();
+//                foo.read();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//
+//        t1.setName("t1");
+//        t2.setName("t2");
+//        t1.start();
+//        t2.start();
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(() -> {
+                try {
+                    foo.read();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            t.setName("thread" + i);
+            t.start();
+        }
     }
 }
