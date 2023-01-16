3c553d077e589f5ae241ca7b117b2117c9c29df2
rukawa <weng.jiaxing@iwhalecloud.com>
2023-01-16 16:54:13 +0800

    引用指向问题

--- src/main/java/com/asiainfo/web/entity/User.java
+++ src/main/java/com/asiainfo/web/entity/User.java
@@ -66,4 +66,16 @@ public class User {
 //    public int compareTo(User that) {
 //        return this.getUserId().compareTo(that.getUserId());
 //    }
+
+    public static void main(String[] args) {
+        User u1 = new User();
+        u1.setUsername("tom");
+        // u2的引用指向u1第一次的堆内存地址
+        User u2 = u1;
+        System.out.println(u1.hashCode());
+        u1 = new User();
+        //u1.setUsername("mike");
+        System.out.println(u1.hashCode());
+        System.out.println(u2.hashCode());
+    }
 }
