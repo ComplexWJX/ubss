a6d1dcc8eb1f90c31b0c322c5f3bb48c5c3433d9
wengjx <weng.jiaxing@iwhalecloud.com>
2023-03-15 18:03:16 +0800

    分层时间轮，待完成

--- src/main/java/com/asiainfo/java/base/TimerWheel.java
+++ src/main/java/com/asiainfo/java/base/TimerWheel.java
@@ -3,14 +3,17 @@ package com.asiainfo.java.base;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.function.Function;
 
 /**
  * [时间轮定时器实现]
- *
- * 简单定时器实现，抽象一个60s的秒针表，通过delay延迟计算出执行所在位置index，将任务存放在对应数组位置上，循环获取数组元素，如果取得任务则执行
+ * <p>
+ * 简单定时器实现，用3个数组抽象组成时钟，通过delay延迟计算出执行所在位置index，将任务存放在对应数组位置上，循环获取数组元素，如果取得任务则执行
+ * 时针：0 1 2 ... 24
+ * 分针：0 1 2 ... 60
+ * 秒针：0 1 2 ... 60
  *
  * @author rukawa
  * Created on 2023/03/15 9:48 by rukawa
@@ -19,54 +22,93 @@ public class TimerWheel {
 
     private static Logger logger = LoggerFactory.getLogger(TimerWheel.class);
 
-    private Object [] jobTimerTable = new Object[60];
+    private MyJob[] secondTable = new MyJob[60];
+
+    private MyJob[] minuteTable = new MyJob[60];
+
+    private MyJob[] hourTable = new MyJob[24];
 
     public static void main(String[] args) {
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime time = LocalDateTime.from(pattern.parse("2023-03-15 18:01:43"));
         TimerWheel timerWheel = new TimerWheel();
-        timerWheel.putJob(3, new MyJob());
+        MyJob myJob = new MyJob(time);
+        timerWheel.putJob(myJob);
 
         LocalDateTime start = LocalDateTime.now();
         logger.info("add job at {}", start);
 
         while (true) {
-            Duration duration = Duration.between(start, LocalDateTime.now());
-            int delay = (int) duration.getSeconds();
-            Object job = timerWheel.getJob(delay);
+            MyJob job = timerWheel.getJob();
             if (job != null) {
-                if (job instanceof Function) {
-                    ((Function) job).apply(delay + "s");
 
-                    timerWheel.removeJob(timerWheel.calcIndex(delay));
-                }
+                job.apply("hello");
+
+                timerWheel.removeJob(myJob);
             }
         }
     }
 
-    public void putJob(int delay, Object job) {
+    public void putJob(MyJob job) {
 
-        jobTimerTable[calcIndex(delay)] = job;
+        int hIdx = job.getExecTime().getHour() % hourTable.length;
+
+        hourTable[hIdx] = job;
+
     }
 
-    public void removeJob(int index) {
-
-        jobTimerTable[index] = null;
+    public MyJob getJob() {
+        return getOrReSetJob();
     }
 
-    public int calcIndex(int delay) {
-        return delay % jobTimerTable.length;
+    private MyJob getOrReSetJob() {
+        LocalDateTime now = LocalDateTime.now();
+        int hIdx = now.getHour() % hourTable.length;
+        int mIdx = now.getMinute() % minuteTable.length;
+        int sIdx = now.getSecond() % secondTable.length;
+
+        if (hourTable[hIdx] != null && minuteTable[mIdx] == null) {
+            LocalDateTime execTime = hourTable[hIdx].getExecTime();
+            int mIdxNew = execTime.getMinute() % minuteTable.length;
+            minuteTable[mIdxNew] = hourTable[hIdx];
+        }
+
+        if (minuteTable[mIdx] != null && secondTable[sIdx] == null) {
+            LocalDateTime execTime = minuteTable[mIdx].getExecTime();
+            int sIdxNew = execTime.getSecond() % secondTable.length;
+            secondTable[sIdxNew] = minuteTable[mIdx];
+        }
+
+        return secondTable[sIdx];
     }
 
-    public Object getJob(int delay) {
-        return jobTimerTable[calcIndex(delay)];
+    public void removeJob(MyJob job) {
+        LocalDateTime execTime = job.getExecTime();
+        int hIdx = execTime.getHour() % hourTable.length;
+        int mIdx = execTime.getMinute() % minuteTable.length;
+        int sIdx = execTime.getSecond() % secondTable.length;
+
+        hourTable[hIdx] = null;
+        minuteTable[mIdx] = null;
+        secondTable[sIdx] = null;
     }
 
     static class MyJob implements Function<String, String> {
 
+        private LocalDateTime execTime;
+
+        public MyJob(LocalDateTime execTime) {
+            this.execTime = execTime;
+        }
+
+        public LocalDateTime getExecTime() {
+            return execTime;
+        }
+
         @Override
         public String apply(String s) {
             logger.info("exec successfully at {} ", LocalDateTime.now());
-            System.out.println("");
-            return "exec successfully.";
+            return "str is : {" + s + "} successfully.";
         }
     }
 }
