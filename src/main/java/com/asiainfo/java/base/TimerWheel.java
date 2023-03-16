ada9a49e8431702fb50fd48cccaf2fe3cc1c8726
wengjx <weng.jiaxing@iwhalecloud.com>
2023-03-16 16:40:37 +0800

    完善时间轮方式定时器

--- src/main/java/com/asiainfo/java/base/TimerWheel.java
+++ src/main/java/com/asiainfo/java/base/TimerWheel.java
@@ -10,7 +10,16 @@ import java.util.function.Function;
 /**
  * [时间轮定时器实现]
  * <p>
- * 简单定时器实现，用3个数组抽象组成时钟，通过delay延迟计算出执行所在位置index，将任务存放在对应数组位置上，循环获取数组元素，如果取得任务则执行
+ * 1.分层时间轮定时器实现，用3个数组抽象组成时钟，根据给定执行时间
+ * 2.计算出执行所在位置index，每当指针指到当层格子时，将任务映射并复制到上一层
+ * 3.循环每次根据当前时分秒模运算获取应查找数组index，如果取得任务则执行
+ *
+ * 示例：从15:56:11启动任务，定时到16:13:53执行任务，先将任务放在hourTable的16%24的位置，等到16时，将hourTable16%24位置
+ *      的任务复制到minuteTable的13%60位置，以此类推，再将minuteTable的13%60复制到secondTable的53%60位置，
+ *      每次遍历如果最终查询secondTable在【当前秒数%60】位置有任务则执行成功。
+ *
+ * 时间轮定时器的时间复杂度为O(1)，直接使用链表或者队列，复杂度O(n)
+ *
  * 时针：0 1 2 ... 24
  * 分针：0 1 2 ... 60
  * 秒针：0 1 2 ... 60
@@ -20,23 +29,25 @@ import java.util.function.Function;
  */
 public class TimerWheel {
 
-    private static Logger logger = LoggerFactory.getLogger(TimerWheel.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TimerWheel.class);
 
-    private MyJob[] secondTable = new MyJob[60];
+    private final MyJob[] secondTable = new MyJob[60];
 
-    private MyJob[] minuteTable = new MyJob[60];
+    private final MyJob[] minuteTable = new MyJob[60];
 
-    private MyJob[] hourTable = new MyJob[24];
+    private final MyJob[] hourTable = new MyJob[24];
+
+    private static final DateTimeFormatter PATTERN = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
-        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime time = LocalDateTime.from(pattern.parse("2023-03-15 18:01:43"));
+
+        LocalDateTime time = LocalDateTime.from(PATTERN.parse("2023-03-16 16:16:53"));
+
         TimerWheel timerWheel = new TimerWheel();
         MyJob myJob = new MyJob(time);
         timerWheel.putJob(myJob);
 
-        LocalDateTime start = LocalDateTime.now();
-        logger.info("add job at {}", start);
+        LOGGER.info("add job at {}", PATTERN.format(LocalDateTime.now()));
 
         while (true) {
             MyJob job = timerWheel.getJob();
@@ -107,7 +118,7 @@ public class TimerWheel {
 
         @Override
         public String apply(String s) {
-            logger.info("exec successfully at {} ", LocalDateTime.now());
+            LOGGER.info("executed successfully at {} ", PATTERN.format(LocalDateTime.now()));
             return "str is : {" + s + "} successfully.";
         }
     }
