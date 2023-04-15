1ae97515a72908212435648ea52d78d12bc9a62d
wengjx <weng.jiaxing@iwhalecloud.com>
2023-04-15 16:49:31 +0800

    printf

--- src/main/java/com/asiainfo/java/runtime/RuntimeExecutor.java
+++ src/main/java/com/asiainfo/java/runtime/RuntimeExecutor.java
@@ -8,14 +8,21 @@ import java.nio.charset.Charset;
  * @description
  * @date 2020/3/17 0017
  */
-public class RuntimeExcutor {
-    private static Runtime runtime = Runtime.getRuntime();
+public class RuntimeExecutor {
 
     public static void main(String[] args) {
+        getAvailableProcessors();
+    }
+
+    private static void getAvailableProcessors() {
+        System.out.printf("availableProcessors is %d", Runtime.getRuntime().availableProcessors());
+    }
+
+    private static void execCmd() {
         try {
             String command = "cmd /c ipconfig /all";
-            Process exec = runtime.exec(command);
-            InputStreamReader in = new InputStreamReader(exec.getInputStream(),Charset.forName("GBK"));
+            Process exec = Runtime.getRuntime().exec(command);
+            InputStreamReader in = new InputStreamReader(exec.getInputStream(), Charset.forName("GBK"));
             String line;
             BufferedReader br = new BufferedReader(in);
             while(null != (line = br.readLine())) {
